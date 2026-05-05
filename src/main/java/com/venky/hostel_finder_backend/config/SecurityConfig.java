ac04fdd1b2a0f86bbf12662f0ba018e5cd1003e0
VenkyGandrothu <gandrothuvenky@gmail.com>
2026-05-05 22:29:45 +0530

    implement security

---
+++ src/main/java/com/venky/hostel_finder_backend/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.venky.hostel_finder_backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/hostels/public/**").permitAll()
+                        .anyRequest().authenticated()
+                );
+        return http.build();
+    }
+}
